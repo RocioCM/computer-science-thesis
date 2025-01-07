d5088a9ca1fd3696e0cb06e0723fb10c37831e0d
Rocío Mena <rociocmena@gmail.com>
2025-01-07 19:56:45 -0300

    Hide token

--- code/frontend/common/services/firebase.ts
+++ code/frontend/common/services/firebase.ts
@@ -2,15 +2,7 @@
 import { initializeApp } from 'firebase/app';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCqmz76giVmxdXg2MN4JumPKiZkZ-6jvgQ",
-  authDomain: "blockchain-thesis-ef714.firebaseapp.com",
-  projectId: "blockchain-thesis-ef714",
-  storageBucket: "blockchain-thesis-ef714.firebasestorage.app",
-  messagingSenderId: "247252167729",
-  appId: "1:247252167729:web:fc8aaea1ac6aa642c57910"
-};
-
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG || '{}');
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
