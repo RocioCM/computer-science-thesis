d33ddc51005a2b4a125a51c678e16aba4a52a06c
Rocío Mena <rociocmena@gmail.com>
2025-01-26 19:10:03 -0300

    Implement multiple contracts

---
+++ code/blockchain/contracts/ProductBottlesBatchContract.sol
@@ -0,0 +1,216 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+struct ProductBottlesBatch {
+  uint256 id;
+  uint256 quantity;
+  uint256 availableQuantity;
+  uint256 originBaseBatchId;
+  string trackingCode;
+  address owner;
+  string createdAt;
+  string deletedAt;
+}
+
+struct SoldProductBatch {
+  uint256 id;
+  uint256 quantity;
+  uint256 originProductBatchId;
+  address owner;
+  string createdAt;
+}
+
+interface BaseBottlesBatchContract {
+  function rejectSoldBaseBottles(uint256 batchId, uint256 quantity) external;
+}
+
+interface RecycledMaterialContract {
+  function recycleBaseBottles(uint256 baseBatchId, uint256 quantity) external;
+}
+
+contract ProductBottlesBatchContract {
+  address public contractOwner;
+  mapping(uint256 id => ProductBottlesBatch) public productBottles;
+  uint256 public nextProductBatchId = 1;
+  mapping(uint256 id => SoldProductBatch) public soldBottles;
+  uint256 public nextSoldBatchId = 1;
+  mapping(string trackingCode => uint256 productBatchId)
+    public trackingCodeToBatchId;
+
+  BaseBottlesBatchContract private baseBottlesBatchContract;
+  RecycledMaterialContract private recycledMaterialContract;
+
+  event ProductBatchCreated(uint256 indexed batchId, address indexed owner);
+  event ProductBatchUpdated(
+    uint256 indexed batchId,
+    string indexed trackingCode
+  );
+  event ProductBatchDeleted(uint256 indexed batchId);
+  event ProductBottlesRecycled(uint256 indexed batchId, uint256 quantity);
+  event ProductBottlesSold(
+    uint256 indexed batchId,
+    uint256 quantity,
+    address buyer
+  );
+
+  modifier onlyContractOwner() {
+    require(msg.sender == contractOwner, 'Unauthorized');
+    _;
+  }
+
+  constructor(address _recycledMaterialContract) {
+    recycledMaterialContract = RecycledMaterialContract(
+      _recycledMaterialContract
+    );
+    contractOwner = msg.sender;
+  }
+
+  function setBaseBottlesBatchContract(
+    address _baseBottlesBatchContract
+  ) external {
+    baseBottlesBatchContract = BaseBottlesBatchContract(
+      _baseBottlesBatchContract
+    );
+  }
+
+  function createProductBottlesBatch(
+    uint256 quantity,
+    uint256 originBaseBatchId,
+    address owner,
+    string memory createdAt
+  ) external onlyContractOwner {
+    ProductBottlesBatch memory newProductBatch = ProductBottlesBatch({
+      id: nextProductBatchId,
+      quantity: quantity,
+      availableQuantity: quantity,
+      originBaseBatchId: originBaseBatchId,
+      trackingCode: '',
+      owner: owner,
+      createdAt: createdAt,
+      deletedAt: ''
+    });
+
+    productBottles[nextProductBatchId] = newProductBatch;
+    nextProductBatchId++;
+    emit ProductBatchCreated(nextProductBatchId, owner);
+  }
+
+  function updateTrackingCode(
+    uint256 batchId,
+    string memory code
+  ) external onlyContractOwner {
+    ProductBottlesBatch storage productBatch = productBottles[batchId];
+    require(productBatch.id != 0, 'Product batch does not exist');
+    require(
+      bytes(productBatch.deletedAt).length == 0,
+      'Product batch already deleted'
+    );
+    require(
+      productBatch.availableQuantity == productBatch.quantity,
+      'Product batch already sold'
+    );
+    require(trackingCodeToBatchId[code] == 0, 'Tracking code already in use');
+
+    if (bytes(productBatch.trackingCode).length > 0) {
+      // If the product batch already has a tracking code, remove it from the mapping.
+      trackingCodeToBatchId[productBatch.trackingCode] = 0;
+    }
+
+    // Update the tracking code and add it to the mapping.
+    productBatch.trackingCode = code;
+    trackingCodeToBatchId[code] = batchId;
+    emit ProductBatchUpdated(batchId, code);
+  }
+
+  function deleteTrackingCode(uint256 batchId) external onlyContractOwner {
+    ProductBottlesBatch storage productBatch = productBottles[batchId];
+    require(productBatch.id != 0, 'Product batch does not exist');
+    require(
+      productBatch.availableQuantity == productBatch.quantity,
+      'Product batch already sold'
+    );
+    require(
+      bytes(productBatch.deletedAt).length == 0,
+      'Product batch already deleted'
+    );
+
+    // Remove the tracking code from the mapping.
+    trackingCodeToBatchId[productBatch.trackingCode] = 0;
+    productBatch.trackingCode = '';
+    emit ProductBatchUpdated(batchId, '');
+  }
+
+  function getProductBottlesList(
+    uint256[] memory indexes
+  ) external view returns (ProductBottlesBatch[] memory) {
+    ProductBottlesBatch[] memory batches = new ProductBottlesBatch[](
+      indexes.length
+    );
+    for (uint256 i = 0; i < indexes.length; i++) {
+      batches[i] = productBottles[indexes[i]];
+    }
+    return batches;
+  }
+
+  function rejectBaseBottles(uint256 batchId) external onlyContractOwner {}
+
+  function recycleProductBottle(
+    uint256 batchId,
+    uint256 quantity
+  ) external onlyContractOwner {
+    ProductBottlesBatch memory productBatch = productBottles[batchId];
+    require(
+      productBatch.availableQuantity >= quantity,
+      'Insufficient quantity in batch'
+    );
+    require(
+      bytes(productBatch.deletedAt).length == 0,
+      'Product batch already deleted'
+    );
+
+    recycledMaterialContract.recycleBaseBottles(
+      productBatch.originBaseBatchId,
+      quantity
+    );
+    productBatch.quantity -= quantity;
+    productBatch.availableQuantity -= quantity;
+    emit ProductBottlesRecycled(batchId, quantity);
+  }
+
+  function sellProductBottle(
+    uint256 batchId,
+    uint256 quantity,
+    address buyer,
+    string memory createdAt
+  ) external onlyContractOwner {
+    ProductBottlesBatch memory productBatch = productBottles[batchId];
+    require(
+      productBatch.availableQuantity >= quantity,
+      'Insufficient quantity in batch'
+    );
+    require(
+      bytes(productBatch.deletedAt).length == 0,
+      'Product batch already deleted'
+    );
+
+    productBatch.availableQuantity -= quantity;
+    soldBottles[nextSoldBatchId] = SoldProductBatch({
+      id: nextSoldBatchId,
+      quantity: quantity,
+      originProductBatchId: batchId,
+      owner: buyer,
+      createdAt: createdAt
+    });
+    nextSoldBatchId++;
+    emit ProductBottlesSold(batchId, quantity, buyer);
+  }
+
+  function getProductBottleByCode(
+    string memory trackingCode
+  ) external view returns (ProductBottlesBatch memory) {
+    uint256 batchId = trackingCodeToBatchId[trackingCode];
+    require(batchId != 0, 'Product batch does not exist');
+    return productBottles[batchId];
+  }
+}
---
+++ code/blockchain/contracts/RecycledMaterialContract.sol
@@ -0,0 +1,279 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import '@openzeppelin/contracts/utils/Strings.sol';
+
+struct WasteBottle {
+  uint256 id;
+  uint256 trackingCode;
+  address owner;
+  address creator;
+  uint256 recycledBatchId;
+  string createdAt;
+  string deletedAt;
+}
+
+struct RecycledMaterialBatch {
+  uint256 id;
+  uint256 weight;
+  string size;
+  string materialType;
+  string composition;
+  string extraInfo;
+  address buyerOwner;
+  address creator;
+  uint256[] wasteBottleIds;
+  string createdAt;
+  string deletedAt;
+}
+
+interface BaseBottlesBatchContract {
+  struct Bottle {
+    uint256 weight;
+    string color;
+    uint256 thickness;
+    string shapeType;
+    string originLocation;
+    string extraInfo;
+    string composition;
+  }
+
+  struct BaseBottlesBatch {
+    uint256 id;
+    uint256 quantity;
+    Bottle bottleType;
+    address owner;
+    uint256 soldQuantity;
+    string createdAt;
+    string deletedAt;
+  }
+
+  function baseBottlesBatches(
+    uint256 id
+  ) external view returns (BaseBottlesBatch memory);
+}
+
+contract RecycledMaterialContract {
+  address public contractOwner;
+  mapping(uint256 id => WasteBottle) public wasteBottles;
+  uint256 public nextWasteBottleId = 1;
+  mapping(uint256 id => RecycledMaterialBatch) public recycledMaterials;
+  uint256 public nextRecycledMaterialBatchId = 1;
+
+  BaseBottlesBatchContract private baseBottlesBatchContract;
+
+  modifier onlyContractOwner() {
+    require(msg.sender == contractOwner, 'Unauthorized');
+    _;
+  }
+
+  constructor() {
+    contractOwner = msg.sender;
+  }
+
+  event WasteBottleCreated(uint256 indexed bottleId, address indexed owner);
+  event WasteBottleUpdated(
+    uint256 indexed bottleId,
+    uint256 trackingCode,
+    uint256 recycledBatchId
+  );
+  event WasteBottleDeleted(uint256 indexed bottleId);
+  event RecycledMaterialBatchCreated(
+    uint256 indexed batchId,
+    address indexed creator
+  );
+  event RecycledMaterialBatchUpdated(uint256 indexed batchId);
+  event RecycledMaterialBatchDeleted(uint256 indexed batchId);
+
+  function setBaseBottlesBatchContract(
+    address _baseBottlesBatchContract
+  ) external {
+    baseBottlesBatchContract = BaseBottlesBatchContract(
+      _baseBottlesBatchContract
+    );
+  }
+
+  function createWasteBottle(
+    uint256 trackingCode,
+    address owner,
+    address creator,
+    string memory createdAt
+  ) external onlyContractOwner {
+    WasteBottle memory newWasteBottle = WasteBottle(
+      nextWasteBottleId,
+      trackingCode,
+      owner,
+      creator,
+      0,
+      createdAt,
+      ''
+    );
+    wasteBottles[nextWasteBottleId] = newWasteBottle;
+    emit WasteBottleCreated(nextWasteBottleId, owner);
+    nextWasteBottleId++;
+  }
+
+  function deleteWasteBottle(
+    uint256 bottleId,
+    string memory deletedAt
+  ) external {
+    WasteBottle storage bottle = wasteBottles[bottleId];
+    require(bottle.id != 0, 'Bottle does not exist');
+    require(bytes(bottle.deletedAt).length == 0, 'Bottle already deleted');
+    require(bottle.recycledBatchId == 0, 'Bottle already recycled');
+
+    bottle.deletedAt = deletedAt;
+    emit WasteBottleDeleted(bottleId);
+  }
+
+  function createRecycledMaterialBatch(
+    uint256 weight,
+    string memory size,
+    string memory materialType,
+    string memory composition,
+    string memory extraInfo,
+    address creator,
+    string memory createdAt
+  ) public onlyContractOwner {
+    RecycledMaterialBatch
+      memory newRecycledMaterialBatch = RecycledMaterialBatch(
+        nextRecycledMaterialBatchId,
+        weight,
+        size,
+        materialType,
+        composition,
+        extraInfo,
+        address(0),
+        creator,
+        new uint256[](0),
+        createdAt,
+        ''
+      );
+    recycledMaterials[nextRecycledMaterialBatchId] = newRecycledMaterialBatch;
+    emit RecycledMaterialBatchCreated(nextRecycledMaterialBatchId, creator);
+    nextRecycledMaterialBatchId++;
+  }
+
+  function addWasteBottleToBatch(
+    uint256 batchId,
+    uint256 bottleId
+  ) external onlyContractOwner {
+    RecycledMaterialBatch storage batch = recycledMaterials[batchId];
+    WasteBottle storage bottle = wasteBottles[bottleId];
+    require(batch.id != 0, 'Batch does not exist');
+    require(bottle.id != 0, 'Bottle does not exist');
+    require(bottle.recycledBatchId == 0, 'Bottle already recycled');
+    bottle.recycledBatchId = batchId;
+    batch.wasteBottleIds.push(bottleId);
+  }
+
+  function removeWasteBottleFromBatch(
+    uint256 batchId,
+    uint256 bottleId
+  ) external onlyContractOwner {
+    RecycledMaterialBatch storage batch = recycledMaterials[batchId];
+    WasteBottle storage bottle = wasteBottles[bottleId];
+    require(batch.id != 0, 'Batch does not exist');
+    require(bottle.id != 0, 'Bottle does not exist');
+    require(bottle.recycledBatchId == batchId, 'Bottle not in batch');
+    bottle.recycledBatchId = 0;
+    uint256[] memory newWasteBottleIds = new uint256[](
+      batch.wasteBottleIds.length - 1
+    );
+    uint256 j = 0;
+    for (uint256 i = 0; i < batch.wasteBottleIds.length; i++) {
+      if (batch.wasteBottleIds[i] != bottleId) {
+        newWasteBottleIds[j] = batch.wasteBottleIds[i];
+        j++;
+      }
+    }
+    batch.wasteBottleIds = newWasteBottleIds;
+  }
+
+  function updateRecycledMaterialBatch(
+    uint256 batchId,
+    uint256 weight,
+    string memory size,
+    string memory materialType,
+    string memory composition,
+    string memory extraInfo
+  ) external onlyContractOwner {
+    RecycledMaterialBatch storage batch = recycledMaterials[batchId];
+    require(batch.id != 0, 'Batch does not exist');
+    require(bytes(batch.deletedAt).length == 0, 'Batch already deleted');
+    require(batch.buyerOwner == address(0), 'Batch already sold');
+
+    batch.weight = weight;
+    batch.size = size;
+    batch.materialType = materialType;
+    batch.composition = composition;
+    batch.extraInfo = extraInfo;
+    emit RecycledMaterialBatchUpdated(batchId);
+  }
+
+  function deleteRecycledMaterialBatch(
+    uint256 batchId,
+    string memory deletedAt
+  ) external onlyContractOwner {
+    RecycledMaterialBatch storage batch = recycledMaterials[batchId];
+    require(batch.id != 0, 'Batch does not exist');
+    require(bytes(batch.deletedAt).length == 0, 'Batch already deleted');
+    require(batch.buyerOwner == address(0), 'Batch already sold');
+
+    batch.deletedAt = deletedAt;
+    emit RecycledMaterialBatchDeleted(batchId);
+  }
+
+  function getWasteBottlesList(
+    uint256[] memory indexes
+  ) external view returns (WasteBottle[] memory) {
+    WasteBottle[] memory bottles = new WasteBottle[](indexes.length);
+    for (uint256 i = 0; i < indexes.length; i++) {
+      bottles[i] = wasteBottles[indexes[i]];
+    }
+    return bottles;
+  }
+
+  function getRecycledMaterialBatchesList(
+    uint256[] memory indexes
+  ) external view returns (RecycledMaterialBatch[] memory) {
+    RecycledMaterialBatch[] memory batches = new RecycledMaterialBatch[](
+      indexes.length
+    );
+    for (uint256 i = 0; i < indexes.length; i++) {
+      batches[i] = recycledMaterials[indexes[i]];
+    }
+    return batches;
+  }
+
+  function recycleBaseBottles(uint256 baseBatchId, uint256 quantity) external {
+    BaseBottlesBatchContract.BaseBottlesBatch
+      memory baseBatch = baseBottlesBatchContract.baseBottlesBatches(
+        baseBatchId
+      );
+    require(baseBatch.id != 0, 'Base batch does not exist');
+
+    uint256 newRecycledBatchId = nextRecycledMaterialBatchId;
+    this.createRecycledMaterialBatch(
+      quantity * baseBatch.bottleType.weight,
+      string.concat(Strings.toString(quantity), ' bottles'),
+      string.concat(baseBatch.bottleType.color, ' glass'),
+      baseBatch.bottleType.composition,
+      baseBatch.bottleType.extraInfo,
+      baseBatch.owner,
+      baseBatch.createdAt
+    );
+    this.sellRecycledMaterialBatch(newRecycledBatchId, baseBatch.owner);
+  }
+
+  function sellRecycledMaterialBatch(
+    uint256 batchId,
+    address buyerOwner
+  ) external {
+    RecycledMaterialBatch storage batch = recycledMaterials[batchId];
+    require(bytes(batch.deletedAt).length == 0, 'Batch already deleted');
+    require(batch.buyerOwner == address(0), 'Batch already sold');
+    batch.buyerOwner = buyerOwner;
+  }
+}
