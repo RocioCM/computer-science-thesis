ae873210797fc85f10ab06de9b498e42eb56d7d2
Rocío Mena <rociocmena@gmail.com>
2025-01-26 19:10:29 -0300

    Configure deployment

--- code/blockchain/scripts/deploy.ts
+++ code/blockchain/scripts/deploy.ts
@@ -1,42 +1,95 @@
 import hre, { ethers } from 'hardhat';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import logger from '../utils/logger';
 import { getEnv } from '../utils/env';
+import {
+  BASE_BATCH_CONTRACT_NAME,
+  PRODUCT_CONTRACT_NAME,
+  RECYCLE_CONTRACT_NAME,
+} from '../test/constants';
 
-const CONTRACT_NAME = 'ConsentManager';
+type Contract = Awaited<
+  ReturnType<HardhatRuntimeEnvironment['ethers']['deployContract']>
+>;
+
+const getNetwork = () => getEnv('HARDHAT_NETWORK', 'localhost');
+
+async function deployContract(
+  contractName: string,
+  constructorArguments: string[] = [],
+): Promise<Contract> {
+  const ContractFactory = await ethers.getContractFactory(contractName);
+  const contract = await ContractFactory.deploy(...constructorArguments);
+  const contractAddress = await contract.getAddress();
+
+  // Wait for network confirmation
+  const network = getNetwork();
+  const isRealNetwork = !!network && network !== 'localhost';
+  if (isRealNetwork) {
+    const res = await contract.waitForDeployment();
+    await res.deploymentTransaction()?.wait(5);
+  }
+  logger.success(
+    `Contract ${contractName} deployed on network ${network} at address ${contractAddress}`,
+  );
+
+  return contract as Contract;
+}
+
+async function verifyContract(
+  contractName: string,
+  contract: Contract,
+  constructorArguments: string[] = [],
+) {
+  try {
+    const contractAddress = await contract.getAddress();
+    await hre.run('verify:verify', {
+      address: contractAddress,
+      constructorArguments,
+    });
+    logger.success('Contract', contractName, 'verified!');
+  } catch (error: any) {
+    if (error?.message?.toLowerCase().includes('already verified')) {
+      logger.info('Contract', contractName, 'already verified!');
+    } else {
+      logger.error('Contract', contractName, 'verification failed:', error);
+    }
+  }
+}
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   logger.info('Deploying contracts with the account:', deployer.address);
 
-  const Contract = await ethers.getContractFactory(CONTRACT_NAME);
-  const contract = await Contract.deploy();
-  const contractAddress = await contract.getAddress();
-  logger.info('Contract address:', contractAddress);
-
-  const network = getEnv('HARDHAT_NETWORK', 'localhost');
+  const network = getNetwork();
   const isRealNetwork = !!network && network !== 'localhost';
 
+  // Deploy contracts
+  const recycledBottlesContract = await deployContract(RECYCLE_CONTRACT_NAME);
+  const recycledAddress = await recycledBottlesContract.getAddress();
+
+  const productBottlesContract = await deployContract(PRODUCT_CONTRACT_NAME, [
+    recycledAddress,
+  ]);
+  const productAddress = await productBottlesContract.getAddress();
+
+  const baseBottlesContract = await deployContract(BASE_BATCH_CONTRACT_NAME, [
+    productAddress,
+    recycledAddress,
+  ]);
+
+  // Verify contracts
   if (isRealNetwork) {
-    logger.info('Verifying contract...');
+    logger.info('Verifying contracts...');
 
-    // Wait for network confirmation
-    const res = await contract.waitForDeployment();
-    await res.deploymentTransaction()?.wait(5);
-    logger.success('Contract deployed on network:', network);
-
-    try {
-      await hre.run('verify:verify', {
-        address: contractAddress,
-        constructorArguments: [],
-      });
-      logger.success('Contract verified!');
-    } catch (error: any) {
-      if (error?.message?.toLowerCase().includes('already verified')) {
-        logger.info('Contract already verified!');
-      } else {
-        logger.error('Verification failed:', error);
-      }
-    }
+    await verifyContract(RECYCLE_CONTRACT_NAME, recycledBottlesContract);
+    await verifyContract(PRODUCT_CONTRACT_NAME, productBottlesContract, [
+      recycledAddress,
+    ]);
+    await verifyContract(BASE_BATCH_CONTRACT_NAME, baseBottlesContract, [
+      productAddress,
+      recycledAddress,
+    ]);
   }
   logger.success('Successful deployment!');
 }
